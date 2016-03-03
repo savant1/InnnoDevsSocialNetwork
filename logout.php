c9f35dc7f48a5b9185961b44f10c812aeafc04fd
ferry francois <ferryfrancois17@gmail.com>
2016-03-03 10:35:07 +0000

    ajout de la suppression du token en base de donnee bdd pour eviter une reconnexion auto apres deconnexion :)

--- logout.php
+++ logout.php
@@ -7,20 +7,13 @@
  */
 
     session_start();
-
+    //suppression du token en base de donnee pour le cas de deconnexion du client
     require ('config/database.php');
         $q = $db->prepare("DELETE FROM auth_tokens WHERE user_id = ?");
-        $q->execute([
-                $_SESSION['user_id']
-        ]);
-
+        $q->execute([$_SESSION['user_id']]);
     setcookie('auth','',time()-3600*25*365);
-
     session_destroy();
-
     $_SESSION = [];
-
     header('Location:login.php');
     exit();
-
 ?>
